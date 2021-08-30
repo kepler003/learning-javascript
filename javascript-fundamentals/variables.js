a6e60b4b7ec6786b38b4c7e40b0c6fde2a2b2cd6
Bartłomiej <b.pyzocha@gmail.com>
2021-08-30 21:48:52 +0200

    Add variables.js

---
+++ javascript-fundamentals/variables.js
@@ -0,0 +1,20 @@
+let message;
+message = 'Hello world';
+console.log(message);
+
+let nextMessage = 'Hello other world';
+console.log(nextMessage);
+
+let one, two, three = 3;
+console.log(one, two, three);
+
+let name = 'Bartek',
+    age = 24,
+    town = 'Krakow';
+console.log(name, age, town);
+
+let cityA = 'Krakow';
+let cityB = cityA;
+console.log(cityB);
+cityA = 'Warszawa';
+console.log(cityB);
