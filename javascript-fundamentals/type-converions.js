dd1810d040f84f8c8846459b4939a7a442bd35bb
Bartłomiej <b.pyzocha@gmail.com>
2021-08-30 23:40:07 +0200

    Add type-conversions.js

---
+++ javascript-fundamentals/type-converions.js
@@ -0,0 +1,8 @@
+const boolean = true;
+console.log(typeof boolean);
+
+const newBoolean = String(boolean);
+console.log(typeof newBoolean);
+
+let text = 'Sample text with a 10 in the middle';
+console.log(Number(text));
