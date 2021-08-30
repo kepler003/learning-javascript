5b8af9a6e0db741a2be14f58bc8be2bee9037910
Bartłomiej <b.pyzocha@gmail.com>
2021-08-30 22:01:54 +0200

    Add data-types.js

---
+++ javascript-fundamentals/data-types.js
@@ -0,0 +1,9 @@
+console.log('0', typeof 0);
+console.log('undefined', typeof undefined);
+console.log('null', typeof null);
+console.log(`'hello'`, typeof 'hello');
+console.log('{}', typeof {});
+console.log('10n', typeof 10n);
+console.log('Math', typeof Math);
+console.log('true', typeof true);
+console.log('true / false', typeof true / false);
