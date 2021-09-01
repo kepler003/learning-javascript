e87f52b97f02ea8af0adbb0fb190e346e2db9a37
Bartłomiej <b.pyzocha@gmail.com>
2021-09-01 23:23:01 +0200

    Rename chapter dir

---
+++ Javascript fundamentals/function-expressions.js
@@ -0,0 +1,11 @@
+
+const showText = function() {
+  console.log('Some text');
+}
+
+showText();
+
+console.log(String(showText));
+
+console.log('------------')
+console.log(typeof showText);
---
+++ Javascript fundamentals/nullish-coalescing-operator.js
@@ -0,0 +1,7 @@
+let a = null;
+let b;
+console.log(a ?? b);
+
+let c = true;
+let d;
+console.log(c ?? d);
