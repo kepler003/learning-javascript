6dca250dbe7fcf19385946f8fd7fa9a445e23f97
Bartłomiej <b.pyzocha@gmail.com>
2021-09-21 19:32:16 +0200

    Add array-methods-task-1.js

---
+++ Data types/array-methods-task-1.js
@@ -0,0 +1,13 @@
+
+const camelize = (str) => {
+  return str
+    .split('-')
+    .map((word, index) => {
+      return index === 0 ? word : word[0].toUpperCase() + word.slice(1);
+    })
+    .join('');
+}
+
+console.log(camelize('background-color'));
+console.log(camelize('list-style-image'));
+console.log(camelize('-webkit-transition'));
