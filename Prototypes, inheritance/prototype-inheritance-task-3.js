843a7960f5dce550da317cd009945560fb8d4a28
Bartłomiej <b.pyzocha@gmail.com>
2021-09-26 20:04:50 +0200

    Add prototype-inheritance-task-3.js

---
+++ Prototypes, inheritance/prototype-inheritance-task-3.js
@@ -0,0 +1,26 @@
+
+let hamster = {
+  
+  eat(food) {
+    if(!this.stomach) {
+      this.stomach = [];
+    }
+
+    this.stomach.push(food);
+  }
+};
+
+let speedy = {
+  __proto__: hamster
+};
+
+let lazy = {
+  __proto__: hamster
+};
+
+// This one found the food
+speedy.eat("apple");
+console.log( speedy.stomach ); // apple
+
+// This one also has it, why? fix please.
+console.log( lazy.stomach ); // apple
