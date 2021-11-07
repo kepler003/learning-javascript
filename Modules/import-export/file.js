e400590b5bd80dc709070eefde97371f651f83da
Bartłomiej <b.pyzocha@gmail.com>
2021-11-07 19:53:43 +0100

    Add import export

---
+++ Modules/import-export/file.js
@@ -0,0 +1,23 @@
+
+// export function sayHello(phrase = 'Hello there'){
+//   console.log(phrase);
+// }
+
+
+// export function sayHi() {
+//   console.log('Hi');
+// }
+
+// export const PI = 3.1415;
+
+
+// export default function() {
+//   console.log('Hello there');
+// }
+
+// export const PI = 3.1415;
+
+
+export function sayHi(){
+  console.log('Hi there');
+}
---
+++ Modules/import-export/main.js
@@ -0,0 +1,19 @@
+
+// import { sayHello } from "./file.js";
+// sayHello();
+
+
+// import * as hello from './file.js';
+
+// hello.sayHi();
+// console.log(hello.PI);
+
+
+// import {default as sayHi, PI} from './file.js';
+
+// sayHi();
+// console.log(PI);
+
+
+import {sayHi} from './target.js';
+sayHi();
