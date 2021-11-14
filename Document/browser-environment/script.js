243fbe5182f2a803ee4566654a8fe962a190ec59
Bartłomiej <b.pyzocha@gmail.com>
2021-11-14 14:53:49 +0100

    Add browser environment

---
+++ Document/browser-environment/script.js
@@ -0,0 +1,26 @@
+
+// function sayHi() {
+//   console.log('Say hi');
+// }
+
+// window.sayHi();
+
+// console.log(window.innerHeight);
+
+
+
+// document.body.style.background = '#000';
+// setTimeout(() => document.body.style.background = '', 1000);
+
+
+
+// console.log(navigator.platform);
+// console.log(navigator.userAgent);
+// console.log(location.href);
+// setTimeout(() => location.href = 'https://www.youtube.com/', 2000);
+
+
+
+if(confirm('Go to YouTube?')) {
+  location.href = 'https://www.youtube.com/';
+}
