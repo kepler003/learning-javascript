907dc8be1eade310a929852bca6a23ca49f5b6f4
Bartłomiej <b.pyzocha@gmail.com>
2021-11-17 21:32:00 +0100

    Add dom navigation

---
+++ Document/dom-navigation/script.js
@@ -0,0 +1,25 @@
+
+// console.log(document.documentElement);
+// console.log(document.body);
+// console.log(document.head);
+
+
+
+// for (let i = 0; i < document.body.childNodes.length; i++) {
+//   console.log(document.body.childNodes[i]);
+// }
+
+
+
+// console.log(document.body.hasChildNodes());
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+
+
+// for (const elem of document.body.children) {
+//   console.log(elem);
+// }
+
+
+
