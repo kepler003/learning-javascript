8f1756802f8e184a8171633ba7466abceacd83e3
Bartłomiej <b.pyzocha@gmail.com>
2021-11-26 00:15:13 +0100

    Add styles and classes

---
+++ Document/styles-and-classes/script.js
@@ -0,0 +1,27 @@
+
+// console.log(document.body.className);
+
+// const classList = document.body.classList;
+// for (const cls of classList) {
+//   console.log(cls);
+// }
+
+// classList.add('test');
+// console.log(document.body.className);
+
+// classList.toggle('test');
+// console.log(document.body.className);
+// console.log(classList.contains('test'));
+// classList.toggle('test');
+// console.log(classList.contains('test'));
+
+
+// document.body.style.cssText = 'background-color: red;';
+
+
+// const style = getComputedStyle(document.body);
+// console.log(style.backgroundColor);
+// document.body.style.backgroundColor = 'rgba(255, 0, 0, 1)';
+// console.log(style.backgroundColor);
+
+
