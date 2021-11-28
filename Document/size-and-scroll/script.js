aff3470a10a29e5b3595820341e1e24ba5e48fee
Bartłomiej <b.pyzocha@gmail.com>
2021-11-28 13:58:04 +0100

    Add size and scroll

---
+++ Document/size-and-scroll/script.js
@@ -0,0 +1,31 @@
+
+// const elem = document.getElementById('example');
+// console.log(elem.offsetParent);
+// console.log(elem.offsetTop);
+// console.log(elem.offsetLeft);
+// console.log(elem.scrollHeight);
+// setTimeout(() => console.log(elem.scrollTop), 2000);
+// setTimeout(() => {
+//   elem.scrollTop = 100;
+// }, 3000);
+// elem.effsetTop = 100;
+
+
+// const elem = document.getElementById('example');
+
+// function animateScroll(elem, distance, time) {
+//   const numOfFrames = time / 10;
+//   const step = distance / numOfFrames;
+
+//   const timer = setInterval(() => {
+//     elem.scrollTop = elem.scrollTop + step;
+//     distance = distance - step;
+//     if (distance <= 0) {
+//       clearInterval(timer);
+//     }
+//   }, 10);
+// }
+
+// animateScroll(elem, 150, 1000);
+
+
