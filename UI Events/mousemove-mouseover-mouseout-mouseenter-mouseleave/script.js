f664e5692a74c02e50d7fedc989fc67071bd4265
Bartłomiej <b.pyzocha@gmail.com>
2021-12-08 13:28:54 +0100

    Add mouseover mouseout mouseenter mouseleave

---
+++ UI Events/mousemove-mouseover-mouseout-mouseenter-mouseleave/script.js
@@ -0,0 +1,12 @@
+
+// document.addEventListener('mouseover', function(e) {
+//   console.log('Related target:', e.relatedTarget);
+//   console.log('Target:', e.target);
+//   console.log('-------------------------');
+// });
+
+document.addEventListener('mouseout', function(e) {
+  console.log('Related target:', e.relatedTarget);
+  console.log('Target:', e.target);
+  console.log('-------------------------');
+});
